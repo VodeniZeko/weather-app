26f299c59b8d661619e9beacca7711f3ff486674
VodeniZeko <salexy.es@gmail.com>
2020-06-21 14:07:47 -0700

    my commit

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=47.608013&lon=-122.335167&appid=67b10bbf46e6fecb07c0365d103c7e1f').then(res => console.log(res))
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+axios
+  .get(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=47.608013&lon=-122.335167&appid=67b10bbf46e6fecb07c0365d103c7e1f"
+  )
+  .then(res => console.log(res));
 
 function App() {
   return (
     <div className="App">
-      
-   <h1>weather app in progress</h1>
-      `   
+      <h1>Is this cool or what :D</h1>`
     </div>
   );
 }
