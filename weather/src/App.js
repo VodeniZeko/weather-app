0dcb9bc6d3e0295707f276da5f863a6b40941b4f
ned <nedsuzman1@gmail.com>
2020-06-21 13:53:57 -0700

    Ned is making a website

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=47.608013&lon=-122.335167&appid=67b10bbf46e6fecb07c0365d103c7e1f').then(res => console.log(res))
+
+function App() {
+  return (
+    <div className="App">
+      
+   <h1>weather app in progress</h1>
+      `   
+    </div>
+  );
+}
+
+export default App;
